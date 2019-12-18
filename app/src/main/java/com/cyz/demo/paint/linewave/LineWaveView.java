bec87ad3f94eb50b1a68e9e9a7d77ad35b8d563d
yanzhou.chen <yanzhou.chen@tcl.com>
2019-12-18 19:33:15 +0800

    不能用，但是想提交保存一下，按照第一种方式做出来的失败品

--- app/src/main/java/com/cyz/demo/paint/linewave/LineWaveView.java
+++ app/src/main/java/com/cyz/demo/paint/linewave/LineWaveView.java
@@ -1,6 +1,5 @@
 package com.cyz.demo.paint.linewave;
 
-import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
@@ -12,19 +11,17 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.SurfaceHolder;
-import android.view.WindowManager;
-import android.view.animation.LinearInterpolator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
-public class LineWaveView extends SurfaceView implements SurfaceHolder.Callback {
+public class LineWaveView extends SurfaceView implements SurfaceHolder.Callback,Runnable {
 
     private static final String TAG = "LineWaveView";
 
     private SurfaceHolder mSurfaceHolder;
-    private ExecutorService mExecutorService;
 
     // 绘图的画布
     private Canvas mLineCanvas;
@@ -32,17 +29,63 @@ public class LineWaveView extends SurfaceView implements SurfaceHolder.Callback 
     private Paint mLinePaint;
     // 绘图的路径
     private Path mLinePath;
+    // 声音的分贝数
+    private float mVoiceVolume = 1;
+    // 波浪线上点的集合
+    private List<LinePoint> mLinePoints = new ArrayList<>();
+    // 波浪线的颜色
+    private int[] lineColors;
+    // 标志位
+    private boolean isDrawing = false;
+    private int width;
+    private int height;
 
-    private int[] mLineColors = {0xff728eff, 0xff6fd3ea, 0xff5996ff, 0xff3a68e6, 0xff5fa4ff, 0xff559df7};  // 波浪线的颜色（渐变）
-    private float[] colorPositions = {0f, 0.2f, 0.4f, 0.6f, 0.8f, 1.0f};
+    // 波浪线自定义的参数
+    private int mBgColor;       // 画布的背景色
+    private int mLineWidth;     // 波浪线的宽度（粗细）
+    private float mLineOffset;  // 波浪线偏移的程度
+    private float mLineSpeed;   // 波浪线横向移动的速度
+    private float mLineShake;   // 波浪线初始化振幅
+    private int mPointSize;     // 波浪线上模拟的点数
+    private int mSensitivity;   // 震动敏感度
 
-    public LineWaveView(Context context) {
-        super(context);
-        initData();
+
+    /**
+     * 波浪线上的点Bean
+     */
+    class LinePoint {
+        float x;
+        float y;
+
+        public LinePoint(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "LinePoint{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
     }
 
     public LineWaveView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        isDrawing = true;
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.lineWave);
+        // 从attrs中获取参数值，如果没有自定义设置则设置默认值
+        mBgColor = typedArray.getColor(R.styleable.lineWave_bgColor, getResources().getColor(R.color.white));
+        mLineWidth = typedArray.getInt(R.styleable.lineWave_lineWidth, 8);
+        mLineOffset = typedArray.getFloat(R.styleable.lineWave_lineOffset, 10f);
+        mLineSpeed = typedArray.getFloat(R.styleable.lineWave_lineSpeed, 2f);
+        mLineShake = typedArray.getFloat(R.styleable.lineWave_lineShake, 0.2f);
+        mPointSize = typedArray.getInt(R.styleable.lineWave_pointSize, 200);
+        mSensitivity = typedArray.getInt(R.styleable.lineWave_sensitivity, 5);
+
         initData();
     }
 
@@ -51,47 +94,30 @@ public class LineWaveView extends SurfaceView implements SurfaceHolder.Callback 
      */
     private void initData() {
 
-        mExecutorService = Executors.newCachedThreadPool();
+        // surfaceView初始化
         mSurfaceHolder = getHolder();
         mSurfaceHolder.addCallback(this);
-
+        // 屏幕参数
+        width = getMeasuredWidth();
+        height = getMeasuredHeight();
+        // 波浪线的颜色
+        lineColors = DataUtils.lineColors;
+        // 绘画初始化
         mLineCanvas = new Canvas();
-        mLinePath = new Path();
+        mLineCanvas.drawColor(mBgColor);
 
         mLinePaint = new Paint();
+        mLinePaint.setStrokeWidth(mLineWidth);
+        mLinePaint.setStyle(Paint.Style.FILL);
+        mLinePaint.setAntiAlias(true);
 
+        mLinePath = new Path();
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        mExecutorService.execute(new Runnable() {
-            @Override
-            public void run() {
-                drawLine();
-            }
-        });
-    }
-
-    private void drawLine() {
-        Log.i(TAG, "drawLine: 走了这吗？");
-
-        mLinePaint.setAntiAlias(true);
-        mLinePaint.setStrokeWidth(8);
-
-        // 设置波浪线为渐变色（放在initData中只显示最后一个颜色）
-        LinearGradient linearGradient = new LinearGradient(0, getMeasuredHeight(), getMeasuredWidth(), 0, mLineColors, colorPositions, Shader.TileMode.CLAMP);
-        mLinePaint.setShader(linearGradient);
-
-        try {
-            mLineCanvas = mSurfaceHolder.lockCanvas();
-            mLineCanvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mLinePaint);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (null != mLineCanvas) {
-                mSurfaceHolder.unlockCanvasAndPost(mLineCanvas);
-            }
-        }
+        isDrawing = true;
+        new Thread(this).start();
     }
 
     @Override
@@ -101,7 +127,121 @@ public class LineWaveView extends SurfaceView implements SurfaceHolder.Callback 
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
+        isDrawing = false;
+    }
 
+    @Override
+    public void run() {
+        long startTime = System.currentTimeMillis();
+        while (isDrawing && mSurfaceHolder != null) {
+            initLine(startTime, System.currentTimeMillis());
+        }
+    }
+
+    /**
+     * 计算出点的位置
+     *
+     * @param startTime
+     * @param nowTime
+     */
+    private void initLine(long startTime, long nowTime) {
+        mLinePoints.clear();
+        // 根据时间偏移
+        long millsPassed = System.currentTimeMillis() - startTime;
+        mLineOffset = millsPassed / 1000f * mLineSpeed;
+        // 波浪线上每个点（pointSize）之间X轴的间距
+        float dx = (float) width / (mPointSize - 1);
+
+        initPositions(mLineOffset, dx, mLineShake, mLinePoints);
+        initDraw();
+    }
+
+    /**
+     * 初始化一条线
+     *
+     * @param offset
+     * @param dx
+     * @param shakeRatio
+     * @param points
+     */
+    private void initPositions(float offset, float dx, float shakeRatio, List<LinePoint> points) {
+        for (int i = 0; i < mPointSize; i++) {
+            // 每个点的x坐标
+            float x = dx * i;
+            // 计算振幅参数：收敛函数 （i-mPointSize/2）^2 + (mPointSize/2)^2
+            float shakeParam = (float) (-Math.pow((i - mPointSize / 2), 2) + Math.pow(mPointSize / 2, 2));
+            // 获得Y轴坐标
+            float y = getLineY(x, offset, shakeParam, shakeRatio);
+            // 存储计算出的每个点
+            points.add(new LinePoint(x, y));
+        }
+    }
+
+    /**
+     * 根据参数计算出当前点（pointSize）的Y轴坐标
+     *
+     * @param x：当前的X轴坐标
+     * @param offset：偏移量
+     * @param shakeParam：振幅参数（收敛）
+     * @param shakeRatio
+     */
+    private float getLineY(float x, float offset, float shakeParam, float shakeRatio) {
+        float dy = (float) Math.sin(Math.toRadians(x) + offset) * shakeParam * shakeRatio * getVoiceVolume();
+        // 波浪线上的点在控件高度中间的位置波动
+        return height / 2 - dy;
+    }
+
+    public void setVoiceVolume(int volume) {
+        mVoiceVolume = volume;
+    }
+
+    private float getVoiceVolume() {
+        // 敏感度越高，声音起振点越低
+        return (mVoiceVolume + mSensitivity) / 10 + 1;
+    }
+
+    /**
+     * 画出波浪线
+     */
+    private void initDraw() {
+
+        try {
+            // 获得当前的Canvas绘图对象（之前的绘图操作如需清除需要使用drawColor()方法）
+            mLineCanvas = mSurfaceHolder.lockCanvas();
+            drawLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (mLineCanvas != null) {
+                // 对画布内容进行提交
+                mSurfaceHolder.unlockCanvasAndPost(mLineCanvas);
+            }
+        }
+    }
+
+    /**
+     * 初始化路径
+     */
+    private void drawLine() {
+        // 从上一个点移动到下一个点
+        for (int i = 1; i < mPointSize; i++) {
+            LinePoint lastPoint = mLinePoints.get(i - 1);
+            LinePoint nextPoint = mLinePoints.get(i);
+            Log.i(TAG, "drawLine: LastPoint = " + lastPoint + ", NextPoint = " + nextPoint);
+
+            int colorIndexMax = lineColors.length - 1;
+            // 将颜色分为等距离的几段，取该段位置的颜色，（%colorIndexMax）放在后面就没有渐变的效果了
+            int lastColor = lineColors[i % colorIndexMax / (mPointSize / colorIndexMax)];
+            int nextColor = lineColors[i % colorIndexMax / (mPointSize / colorIndexMax) + 1];
+            Log.i(TAG, "drawLine: LastColor = " + lastColor + ", NextColor = " + nextColor);
+            // 设置波浪线为渐变色
+            LinearGradient linearGradient = new LinearGradient(0, height / 2, width, height / 2, lastColor, nextColor, Shader.TileMode.CLAMP);
+            mLinePaint.setShader(linearGradient);
+
+            mLineCanvas.drawLine(lastPoint.x, lastPoint.y, nextPoint.x, nextPoint.y, mLinePaint);
+            mLineCanvas.drawLine(lastPoint.x, lastPoint.y, nextPoint.x, nextPoint.y, mLinePaint);
+
+        }
     }
 
 }
